57aa71d08238767ff9598592f91616c8aa97ed77
김기정 <cjdfidrlwjd@naver.com>
2023-09-19 11:00:10 +0900

    :sparkles: App.tsx 딥링크 설정 sieum://oauth/callback 경로로 접근 시 OAuthCallbackHandler로 라우팅 props.route.param으로 쿼리로 전달되는 access/refreshToken 접근 Resolves #4

--- App.tsx
+++ App.tsx
@@ -1,18 +1,44 @@
-import {NavigationContainer} from "@react-navigation/native";
-import Navigation from "~nav/Navigation";
-import {SafeAreaProvider} from "react-native-safe-area-context";
-import {StatusBar} from "react-native";
-import React from "react";
-function App() {
-  return (
-      <SafeAreaProvider>
-          <StatusBar hidden={true} />
-          <NavigationContainer>
-            <Navigation/>
-          </NavigationContainer>
-      </SafeAreaProvider>
-  );
+import {NavigationContainer, PathConfigMap} from '@react-navigation/native';
+import Navigation from '~nav/Navigation';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Linking, StatusBar} from 'react-native';
+import React from 'react';
 
+function App() {
+  const config: {screens: PathConfigMap<object>} = {
+    screens: {
+      Stack: {
+        screens: {
+          OAuthCallbackHandler: 'oauth/callback/:accessToken/:refreshToken',
+        },
+      },
+      NavigationTabBar: {
+        screens: {
+          Home: 'home',
+        },
+      },
+    },
+  };
+  const linking = {
+    prefixes: ['sieum://'],
+    config,
+    subscribe(listener: (url: string) => void) {
+      const onReceiveURL = ({url}: {url: string}) => {
+        console.log('url : ' + url);
+        return listener(url);
+      };
+
+      Linking.addEventListener('url', onReceiveURL);
+    },
+  };
+  return (
+    <SafeAreaProvider>
+      <StatusBar hidden={true} />
+      <NavigationContainer linking={linking}>
+        <Navigation />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
 }
 
 export default App;
--- src/nav/Stack.tsx
+++ src/nav/Stack.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { createNativeStackNavigator} from "@react-navigation/native-stack";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import OAuthCallbackHandler from '~components/OAuthCallbackHandler';
 
 const NativeStack = createNativeStackNavigator();
 
 const Stack = () => {
-    return(
-        <NativeStack.Navigator
-            screenOptions={{
-                presentation: 'modal',
-                headerShown: false,
-            }}>
-        </NativeStack.Navigator>
-    )
-}
+  return (
+    <NativeStack.Navigator
+      screenOptions={{
+        presentation: 'modal',
+        headerShown: false,
+      }}>
+      <NativeStack.Screen
+        name="OAuthCallbackHandler"
+        component={OAuthCallbackHandler}
+      />
+    </NativeStack.Navigator>
+  );
+};
 
 export default Stack;

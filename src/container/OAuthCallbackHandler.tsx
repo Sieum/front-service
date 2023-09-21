a77ca66d9140fcd7fe95938594b47156bdd0b0c1
김기정 <cjdfidrlwjd@naver.com>
2023-09-21 11:15:34 +0900

    :sparkles: 로그인 후 딥링크를 통해 전달 받은 토큰을 URL에서 파싱하여 secure-storage에 저장
    Related to #4

--- src/container/OAuthCallbackHandler.tsx
+++ src/container/OAuthCallbackHandler.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View} from 'react-native';
+import RNSecureStorage, {ACCESSIBLE} from 'rn-secure-storage';
 
-const OAuthCallbackHandler = (props: any) => {
-  console.log('props.route.params', props.route.params);
+const OAuthCallbackHandler = ({route}) => {
+  const [accessToken, setAccessToken] = useState<string>('');
+  const [refreshToken, setRefreshToken] = useState<string>('');
+
+  useEffect(() => {
+    setAccessToken(route.params.accessToken);
+    setRefreshToken(route.params.refreshToken);
+  }, []);
+
+  useEffect(() => {
+    if (accessToken && refreshToken) {
+      saveTokenToSecureStore('accessToken', accessToken);
+      saveTokenToSecureStore('refreshToken', refreshToken);
+    }
+  }, [accessToken, refreshToken]);
+
+  const saveTokenToSecureStore = (type: string, token: string) => {
+    RNSecureStorage.set(type, token, {
+      accessible: ACCESSIBLE.WHEN_UNLOCKED,
+    }).then(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      },
+    );
+  };
+
   return (
     <View>
       <Text>OAuthCallbackHandler</Text>

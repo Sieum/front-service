27ce7626b512133754f5afd0ceddd28b039e92f9
정의석 <euiseok96@gmail.com>
2023-09-21 16:48:43 +0900

    :rewind: 잘못 커밋한 내용 리와인드

--- src/nav/Navigation.tsx
+++ src/nav/Navigation.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import NavigationTabBar from '~nav/NavigationTabBar';
 import Stack from '~nav/Stack';
-import MyProfile from 'src/presentation/profile/MyProfile';
 
 const Nav = createNativeStackNavigator();
 
@@ -13,7 +12,6 @@ const Navigation = () => {
         presentation: 'modal',
         headerShown: false,
       }}>
-      <Nav.Screen name="MyProfile" component={MyProfile} />
       <Nav.Screen name="Stack" component={Stack} />
       <Nav.Screen name="NavigationTabBar" component={NavigationTabBar} />
     </Nav.Navigator>

e3d42513c4ec7054215963ce7acb4c144300aed0
김기정 <cjdfidrlwjd@naver.com>
2023-09-22 09:13:58 +0900

    :art: URL에서 토큰을 파싱하여 secure-store에 저장한 뒤 recoil atom 전역 state에도 저장.
    Related to #4

--- src/container/OAuthCallbackHandler.tsx
+++ src/container/OAuthCallbackHandler.tsx
@@ -1,34 +1,37 @@
 import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 import RNSecureStorage, {ACCESSIBLE} from 'rn-secure-storage';
+import {useSetRecoilState} from 'recoil';
+import {AccessTokenAtom, RefreshTokenAtom} from '~recoil/TokenAtom';
 
 const OAuthCallbackHandler = ({route}) => {
   const [accessToken, setAccessToken] = useState<string>('');
   const [refreshToken, setRefreshToken] = useState<string>('');
+  const setAccessTokenAtom = useSetRecoilState(AccessTokenAtom);
+  const setRefreshTokenAtom = useSetRecoilState(RefreshTokenAtom);
 
+  // DeepLink URL에서 access/refreshToken 추출
   useEffect(() => {
     setAccessToken(route.params.accessToken);
     setRefreshToken(route.params.refreshToken);
   }, []);
 
+  // 추출한 access/refreshToken을 SecureStore 및 Atom 전역 state에 저장
   useEffect(() => {
     if (accessToken && refreshToken) {
-      saveTokenToSecureStore('accessToken', accessToken);
-      saveTokenToSecureStore('refreshToken', refreshToken);
+      saveTokenToSecureStore('accessToken', accessToken).then(() => {
+        setAccessTokenAtom(accessToken);
+      });
+      saveTokenToSecureStore('refreshToken', refreshToken).then(() => {
+        setRefreshTokenAtom(refreshToken);
+      });
     }
   }, [accessToken, refreshToken]);
 
-  const saveTokenToSecureStore = (type: string, token: string) => {
-    RNSecureStorage.set(type, token, {
+  const saveTokenToSecureStore = async (type: string, token: string) => {
+    await RNSecureStorage.set(type, token, {
       accessible: ACCESSIBLE.WHEN_UNLOCKED,
-    }).then(
-      res => {
-        console.log(res);
-      },
-      err => {
-        console.log(err);
-      },
-    );
+    });
   };
 
   return (

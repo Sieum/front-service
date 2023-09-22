6d027674c1548e6985f5b337b338d2c6e4647e30
오승기 <dhtmdrl2004@knou.ac.kr>
2023-09-22 16:34:30 +0900

    :sparkles: 공통 상단 바 컴포넌트 구현 상단 컴포넌트 구현 완료 Resolves #10

--- src/presentation/playList/PlayList.tsx
+++ src/presentation/playList/PlayList.tsx
@@ -1,12 +1,60 @@
 import React from "react";
-import { View, Text} from "react-native";
+import { ScrollView, View, StyleSheet } from "react-native";
+import { Text, Appbar, Divider } from "react-native-paper";
 
-const Home = () => {
-    return(
-        <View>
-            <Text>PlayList</Text>
-        </View>
-    )
-}
 
-export default Home;
+
+const Topbar = ({title}) => {
+    const topbarStyle = StyleSheet.create({
+        topbar: {
+            alignItems: "center",
+            textAlign: 'center',
+        },
+        title: {
+            flex: 1,
+            alignItems: "center",
+            textAlign: 'center',
+        },
+    });
+
+    return (
+        <>
+            <Appbar.Header style={topbarStyle.topbar}>
+                <Appbar.Content title={title}  style={topbarStyle.title}/>
+            </Appbar.Header>
+            <Divider />
+        </>
+    );
+};
+
+const ListBox = () => {
+    const ListBoxStyle = StyleSheet.create({
+        title: {
+            fontWeight: "bold",
+            margin: 10,
+        },
+    });
+
+    return (
+        <>
+            <Text variant="titleLarge" style={ListBoxStyle.title}>"사용자이름"님의 플레이리스트</Text>
+        </>
+    );
+};
+
+const Playlist = () => {
+    const mainStyle = StyleSheet.create({
+        background: {
+            backgroundColor: "white",
+        },
+    });
+
+    return (
+        <ScrollView style={mainStyle.background}>
+            <Topbar title={"플레이리스트"}/>
+            <ListBox />
+        </ScrollView>
+    );
+};
+
+export default Playlist;

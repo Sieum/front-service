79989bd6164720d15559099f14a4fc7b63e1399d
정의석 <euiseok96@gmail.com>
2023-09-24 23:09:11 +0900

    :sparkles: 구글 맵 컴포넌트 추가 및 랜더링

---
+++ src/presentation/home/GoogleMap.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+
+const GoogleMap = () => {
+  return (
+    <View style={{flexDirection: 'row', width: '100%', height: '100%'}}>
+      <MapView
+        style={{flex: 1}}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
+};
+
+export default GoogleMap;

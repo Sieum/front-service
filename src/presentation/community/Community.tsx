40aae62533aed51b28f234970814a1183e21996f
오승기 <dhtmdrl2004@knou.ac.kr>
2023-10-04 00:29:26 +0900

    :art: 커뮤니티 화면 구조 및 디자인 변경 완료 상단바 구현 완료 게시물 컴포넌트 분리 구현 완료 전반적인 디자인 수정 완료 Resolves #9

--- src/presentation/community/Community.tsx
+++ src/presentation/community/Community.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  FlatList,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
+import { View, FlatList, Image, StyleSheet, Dimensions } from 'react-native';
+import { Card, FAB, IconButton, Text } from 'react-native-paper';
+import Topbar from '~components/Topbar';
 
 // 게시물 아이템의 타입 정의
 interface PostItem {
@@ -15,6 +10,8 @@ interface PostItem {
   userProfileImage: number; // 이미지 파일 경로로 수정
   postImage: number; // 이미지 파일 경로로 수정
   caption: string;
+  like: string;
+  reply: string;
 }
 
 const postData: PostItem[] = [
@@ -23,7 +20,9 @@ const postData: PostItem[] = [
     username: 'user1',
     userProfileImage: require('src/static/images/profileimage.png'),
     postImage: require('src/static/images/cover.png'),
-    caption: '게시글 내용 1',
+    caption: '게시글 내용 1dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd',
+    like: '232',
+    reply: '10',
   },
   {
     id: '2',
@@ -31,106 +30,149 @@ const postData: PostItem[] = [
     userProfileImage: require('src/static/images/profileimage.png'),
     postImage: require('src/static/images/cover.png'),
     caption: '게시글 내용 2',
+    like: '14',
+    reply: '0',
   },
 ];
 
-const CommunityContainer: React.FC = () => {
-  const renderPostItem = ({item}: {item: PostItem}) => (
-    // 게시글
-    <View style={styles.postContainer}>
-      <View style={styles.userProfileContainer}>
-        <Image source={item.userProfileImage} style={styles.userProfileImage} />
-        <Text style={styles.username}>{item.username}</Text>
-      </View>
-      <Image source={item.postImage} style={styles.postImage} />
-      <Text style={styles.caption}>{item.caption}</Text>
+const PostComponent = ({item}: {item: PostItem}) => {
+  const styles = StyleSheet.create({
+    card: {
+      alignSelf: "center",
+      width: Dimensions.get("window").width,
+      marginBottom: 20,
+      backgroundColor: "white",
+      borderRadius: 0,
+    },
+    cover: {
+      alignSelf: "center",
+      borderRadius: 0,
+      width: Dimensions.get("window").width,
+      height: Dimensions.get("window").width,
+      aspectRatio: 1,
+      resizeMode: "cover",
+    },
+    content: {
+      margin: 5,
+    },
+    userProfileContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      margin: 5,
+    },
+    userProfileImage: {
+      width: 40,
+      height: 40,
+      borderRadius: 20,
+      marginRight: 10,
+    },
+    username: {
+      fontWeight: 'bold',
+    },
+    postImage: {
+      width: '100%',
+      height: 300,
+      resizeMode: 'cover',
+      marginBottom: 8,
+    },
+    actionButtons: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: 8,
+    },
+    actionButton: {
+      flex: 1,
+      alignItems: 'center',
+      paddingVertical: 8,
+      backgroundColor: '#FCD34D',
+      borderRadius: 4,
+      marginHorizontal: 4,
+    },
+    actionButtonText: {
+      color: 'white',
+    },
 
-      {/* 좋아요 및 댓글 버튼 */}
-      <View style={styles.actionButtons}>
-        <TouchableOpacity
-          onPress={() => handleLike(item.id)}
-          style={styles.actionButton}>
-          <Text style={styles.actionButtonText}>좋아요</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => handleComment(item.id)}
-          style={styles.actionButton}>
-          <Text style={styles.actionButtonText}>댓글</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-
-  const handleLike = (postId: string) => {
-    // 좋아요 버튼을 눌렀을 때의 로직
-    // postId를 사용하여 해당 게시물에 대한 좋아요 상태를 업데이트하거나 API 호출
-    console.log(`${postId}번 게시글 좋아요`);
-  };
-
-  const handleComment = (postId: string) => {
-    // 댓글 버튼을 눌렀을 때의 로직
-    // postId를 사용하여 해당 게시물에 댓글 창 열기
-    console.log(`${postId}번 게시글 댓글 창`);
-  };
+    buttonContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    text: {
+      marginVertical: 5,
+    },
+  });
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={postData}
-        keyExtractor={item => item.id}
-        renderItem={renderPostItem}
+    <>
+      <Card mode="contained" style={styles.card}>
+        <View style={styles.userProfileContainer}>
+          <Image source={item.userProfileImage} style={styles.userProfileImage} />
+          <Text variant="bodyLarge" style={styles.username}>{item.username}</Text>
+        </View>
+
+        <Card.Cover style={styles.cover} source={item.postImage} />
+        <View style={styles.buttonContainer}>
+          <IconButton
+            icon="cards-heart-outline"
+            iconColor="#FCD34D"
+            size={30}
+            onPress={() => {
+              // 좋아요 기능
+            }}
+          />
+          <IconButton
+            icon="comment-outline"
+            iconColor="#FCD34D"
+            size={30}
+            onPress={() => {
+              // 댓글창 열기
+            }}
+          />
+        </View>
+        <Card.Content style={styles.content}>
+          <Text variant="bodyMedium" style={styles.text}>좋아요 x개</Text>
+          <Text variant="bodyMedium" style={styles.text}>{item.caption}</Text>
+          <Text variant="bodyLarge" style={styles.text}>댓글 x개 보기</Text>
+          <Text variant="bodyMedium" style={styles.text}>몇시간 전</Text>
+        </Card.Content>
+      </Card>
+    </>
+  );
+};
+
+const CommunityContainer: React.FC = () => {
+
+  return (
+    <View style={styles.mainBg}>
+      <Topbar title={"커뮤니티"} />
+      <View>
+        <FlatList
+          data={postData}
+          keyExtractor={item => item.id}
+          renderItem={PostComponent}
+        />
+      </View>
+      <FAB
+        icon="plus"
+        color="white"
+        style={styles.fab}
+        onPress={() => {
+          // 게시글 작성 창 이동
+        }}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
+  mainBg: {
+    backgroundColor: "white",
   },
-  postContainer: {
-    marginBottom: 16,
-  },
-  userProfileContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 8,
-  },
-  userProfileImage: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    marginRight: 8,
-  },
-  username: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  postImage: {
-    width: '100%',
-    height: 300,
-    resizeMode: 'cover',
-    marginBottom: 8,
-  },
-  caption: {
-    fontSize: 16,
-  },
-  actionButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 8,
-  },
-  actionButton: {
-    flex: 1,
-    alignItems: 'center',
-    paddingVertical: 8,
-    backgroundColor: '#FCD34D',
-    borderRadius: 4,
-    marginHorizontal: 4,
-  },
-  actionButtonText: {
-    color: 'white',
+  fab: {
+    position: 'absolute',
+    margin: 20,
+    right: 0,
+    bottom: 70,
+    backgroundColor: "#FCD34D",
   },
 });
 
